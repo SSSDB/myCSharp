ef43bda972ae6ae5b8206472aab1970b963d1ec9
SSSDB <1060265587@qq.com>
2020-05-31 02:23:44 +0800

    第四次作业

---
+++ HW04test01/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            for (int x = 0; x < 10; x++) {
+                list.Add(x);
+            }
+            list.ForEach(x => Console.WriteLine(x));
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int sum = 0;
+            list.ForEach(x => { if (max < x) max = x; });
+            Console.WriteLine($"max:{max}");
+            list.ForEach(x => { if (min > x) min = x; });
+            Console.WriteLine($"min:{min}");
+            list.ForEach(x => sum+=x);
+            Console.WriteLine($"sum:{sum}");
+        }
+        public class Node<T> { 
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t) {
+                Next = null;
+                Data = t;
+            }
+        }
+
+        public class GenericList<T> {
+            private Node<T> head;
+            private Node<T> tail;
+            public GenericList() {
+                tail = head = null;
+            }
+            public Node<T> Head { get => head; }
+            public void Add(T t) {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+
+            public void ForEach(Action<T> action)
+            {
+                Node<T> n = this.head;
+                while (n != null) {
+                    action(n.Data);
+                    n = n.Next;
+                }
+            }
+        }
+    }
+}
---
+++ HW04test02/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+        public delegate void ClockHandler(object sender, DateTime dataTime);
+        public class AlarmClock
+        {
+            public event ClockHandler Tick;
+            public event ClockHandler Alarm;
+            private DateTime alarmTime;
+            public DateTime AlarmTime
+            {
+                get => alarmTime;
+                set => alarmTime = value;
+            }
+            public void Run()
+            {
+                Console.WriteLine("开始运行！");
+                while (true)
+                {
+                    Tick(this, DateTime.Now);
+                    if (DateTime.Now.ToString() == AlarmTime.ToString())
+                    {
+                        Alarm(this, DateTime.Now);
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+
+    public class MyClock
+    {
+        public AlarmClock alarmClock = new AlarmClock();
+        public MyClock()
+        {
+            DateTime alarmTime = new DateTime();
+            int year, month, day, hour, minute, second;
+            Console.WriteLine("请输入设定闹钟时间（按行输入年、月、日、时、分、秒，用enter分开）：");
+            try
+            {
+                year = int.Parse(Console.ReadLine());
+                month = int.Parse(Console.ReadLine());
+                day = int.Parse(Console.ReadLine());
+                hour = int.Parse(Console.ReadLine());
+                minute = int.Parse(Console.ReadLine());
+                second = int.Parse(Console.ReadLine());
+                alarmTime = new DateTime(year, month, day, hour, minute, second);
+                Console.WriteLine($"You have set the alarm time as {alarmTime}");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Error:the time you enter is out of range");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error:the format of time you enter is wrong");
+            }
+            // alarmClock.AlarmTime = new DateTime(2020, 3, 17, 16, 26, 0);
+            alarmClock.AlarmTime = alarmTime;
+            alarmClock.Tick += new ClockHandler(Alarm_tick);
+            alarmClock.Alarm += Alarm_alarm;
+        }
+        void Alarm_tick(object sender, DateTime dateTime)
+        {
+            Console.WriteLine($"滴答、滴答、  现在时间：{dateTime.ToString()}");
+        }
+        void Alarm_alarm(object sender, DateTime dateTime)
+        {
+            Console.WriteLine($"叮铃、叮铃、  现在时间：{dateTime.ToString()}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClock myClock = new MyClock();
+            myClock.alarmClock.Run();
+        }
+    }
+}
